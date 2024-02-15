cf1ae3ea15362502499d4d66007964bfb7635d80
Xecades <i@xecades.xyz>
2024-02-15 12:09:54 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Xecades RPi Camera
+
+A camera GUI designed for 3.5inch RPi Display (Resolution: 480x320).
+
+## Hardware
+
+ - Model: Raspberry Pi 3B
+ - Screen: [3.5inch RPi Display](http://www.lcdwiki.com/3.5inch_RPi_Display)
+ - Camera: [Raspberry Pi Camera Rev 1.3](https://www.raspberrypi.org/products/camera-module-v1/)
---
+++ util.py
@@ -0,0 +1,34 @@
+import os
+import time
+
+
+def __console__(color, message):
+    NC = "\033[0m"
+    timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"{color}[{timestamp}] {message}{NC}")
+
+
+def log(message): return __console__("\033[0;32m", message)
+def error(message): return __console__("\033[0;31m", message)
+
+
+def ensureFolder(path):
+    if not os.path.exists(path):
+        log(f"{path} does not exist, creating")
+        os.makedirs(path)
+
+
+def fetchLocalImages(path, extension=".jpg"):
+    log("Fetching local images")
+
+    if not os.path.exists(path):
+        error(f"{path} does not exist")
+        return []
+
+    dist = os.listdir(path)
+    files = [f for f in dist if os.path.isfile(os.path.join(path, f))]
+    jpg_files = [f for f in files if f.lower().endswith(extension)]
+    full_paths = [os.path.join(path, f) for f in jpg_files]
+    sorted_files = sorted(full_paths, key=os.path.getmtime)
+
+    return sorted_files
