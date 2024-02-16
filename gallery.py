dc03f591836beaff68b454dcfd991d4bfeda3f0f
Xecades <i@xecades.xyz>
2024-02-16 14:29:14 +0800

    feat: a lot

--- gallery.py
+++ gallery.py
@@ -11,25 +11,51 @@ from PyQt5.QtGui import QPixmap
 class GalleryView:
     def __init__(self, parent):
         self.pos = 0
+        self.readyToDelete = False
+
+        self.pa = parent
 
         self.preview = PreviewLabel(parent)
         self.info = InfoLabel(parent)
 
-        self.exit = RawButton(parent, 0, 5, "EXIT", parent.exitGallery)
-        self.prev = RawButton(parent, 1, 5, "PREV", self.navigate(-1))
-        self.next = RawButton(parent, 2, 5, "NEXT", self.navigate(1))
-        self.send = RawButton(parent, 3, 5, "SEND", self.send)
-        self.delete = RawButton(parent, 4, 5, "DELETE", self.delete)
+        self.exit = RawButton(parent, 0, 5, "Exit", parent.exitGallery)
+        self.prev = RawButton(parent, 1, 5, "Previous", self.navigate(-1))
+        self.next = RawButton(parent, 2, 5, "Next", self.navigate(1))
+        self.send = RawButton(parent, 3, 5, "Send", self.sendImg)
+        self.delete = RawButton(parent, 4, 5, "Delete", self.deleteImg)
 
         self.refreshList()
         self.update()
         self.show()
 
-    def send(self):
+    def sendImg(self):
         pass
 
-    def delete(self):
-        pass
+    def deleteImg(self):
+        if self.readyToDelete:
+            self.readyToDelete = False
+            self.delete.setStyle(BUTTON_COLOR)
+            self.delete.setText("Delete")
+
+            img = self.imgs[self.pos]
+            
+            util.log(f"Deleting image {img} and its DNG file")
+
+            os.remove(img)
+            os.remove(img.replace(".jpg", ".dng"))
+
+            self.refreshList()
+            self.pos = min(self.pos, len(self.imgs) - 1)
+            self.update()
+
+            new_thumb = self.imgs[0] if self.imgs else None
+            self.pa.cameraView.button.updateThumbnail(new_thumb)
+
+            return
+
+        self.readyToDelete = True
+        self.delete.setStyle(BUTTON_WARN_COLOR)
+        self.delete.setText("Confirm")
 
     def navigate(self, direction):
         def wrap():
@@ -46,15 +72,18 @@ class GalleryView:
         self.preview.loadImage(img)
         self.info.updateInfo(self.pos + 1, len(self.imgs), img)
 
-    def __apply__(self, fn):
-        getattr(self.preview, fn)()
-        getattr(self.info, fn)()
+        for btn in [self.prev, self.next, self.send, self.delete]:
+            btn.setDisabled(not self.imgs)
 
-        for button in [self.exit, self.prev, self.next, self.send, self.delete]:
-            getattr(button, fn)()
-    
-    def show(self): self.__apply__("show")
-    def hide(self): self.__apply__("hide")
+    def __apply_all__(self, fn, *args):
+        getattr(self.preview, fn)(*args)
+        getattr(self.info, fn)(*args)
+
+        for btn in [self.exit, self.prev, self.next, self.send, self.delete]:
+            getattr(btn, fn)(*args)
+
+    def show(self): self.__apply_all__("show")
+    def hide(self): self.__apply_all__("hide")
 
 
 class PreviewLabel(QLabel):
@@ -86,8 +115,13 @@ class InfoLabel(QLabel):
 
         util.log(f"Rendering gallery info, width={W}, height={H}, x={X}, y={Y}")
 
+        style = f"""
+            color: {TEXT_COLOR};
+            background-color: {LABEL_COLOR};
+            padding: 2px;
+            font-size: 11px;"""
         self.setGeometry(X, Y, W, H)
-        self.setStyleSheet(f"color: {TEXT_COLOR}")
+        self.setStyleSheet(style)
 
     def updateInfo(self, nth, total, img=None):
         if not img:
@@ -104,12 +138,15 @@ class InfoLabel(QLabel):
         iso = int(exif[34855])
         aperture = round(float(exif[33437]), 1)
         shutter = round(1 / float(exif[33434]))
+        width = exif[256]
+        height = exif[257]
+
         model = exif[272]
 
         content = f"""
             <b>{nth}/{total} {filename}</b> ({size})
+            <br>ISO {iso} | f/{aperture} | 1/{shutter}s | {width}x{height}
             <br>{date}
-            <br>ISO {iso} | f/{aperture} | 1/{shutter}s
             <br>Captured by {model}"""
         self.setText(content)
 
@@ -125,11 +162,22 @@ class RawButton(QPushButton):
 
         util.log(f"Rendering gallery button {text}, width={W}, height={H}, x={X}, y={Y}")
 
+        self.style = """
+            QPushButton {{
+                color: {};
+                background-color: {};
+                border: 1px solid {};
+                font-size: 20px;
+                font-weight: bold;
+            }}
+            QPushButton:pressed {{
+                background-color: {};
+            }}"""
+
         self.setGeometry(X, Y, W, H)
-        self.setStyleSheet(f"""
-            color: {TEXT_COLOR};
-            background-color: {BUTTON_COLOR};
-            font-size: 20px;
-            font-weight: bold;""")
+        self.setStyle(BUTTON_COLOR)
         self.setText(text)
         self.clicked.connect(action)
+
+    def setStyle(self, BGColor):
+        self.setStyleSheet(self.style.format(TEXT_COLOR, BGColor, BORDER_COLOR, BUTTON_ACTIVE_COLOR))
